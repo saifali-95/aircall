30300b585760f7a03b5669f08e0740b1925d8be5
Saif Ali <saifali7646@gmail.com>
2022-01-27 11:23:17 -0500

    initial layout of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Header from './Header.jsx';
+import Header from "./Header.jsx";
 
 const App = () => {
   return (
-    <div className='container'>
-      <Header/>
-      <div className="container-view">Some activities should be here</div>
+    <div className="container">
+      <Header />
+      <div className="container-view">
+        <div className="tabs">
+          <div className="left-tab">
+            <h1>Activity Feed</h1>
+          </div>
+          <div className="right-tab">
+            <div className="all-calls">All Calls</div>
+            <div className="archived-calls">Archived Calls</div>
+          </div>
+        </div>
+        <div className="calls-list-container">
+          <div className="call-container">
+            <div className="date">
+              ................ 17- July - 2020 .....................
+            </div>
+            <h1>Call 1</h1>
+          </div>
+        </div>
+      </div>
+      <div className="footer-container-view">
+        <div className="footer-icons">
+          <i class="fas fa-phone-alt fa-lg"></i>
+          <i class="far fa-user fa-lg"></i>
+          <i class="fas fa-cog fa-lg"></i>
+          <i class="fas fa-record-vinyl fa-lg"></i>
+        </div>
+      </div>
     </div>
   );
 };
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
 
 export default App;
