0bf2c004961c92f588e09247b76bdeeb4131c8a8
Saif Ali <saifali7646@gmail.com>
2022-01-27 19:09:42 -0500

    update order of the data and call display

--- src/Calls.jsx
+++ src/Calls.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import "./css/calls.css";
 
-const Calls = (props) => {
+const Calls = props => {
+  
+  let d = new Date(props.time);
+ 
   return (
-    <div>
-      
+    <div className="call-container">
+      <div className="date">
+      ----------------------------------------{d.toLocaleDateString()}----------------------------------------
+      </div>
+      <div className="call-item">
+        <div className="call-left">
+          <i className="fas fa-phone-alt"></i>
+        </div>
+        <div className="call-center">
+          <div className="call-number">
+            {props.from}
+          </div>
+          <div className="call-detail">
+            {props.via}
+          </div>
+        </div>
+        <div className="call-right">
+          {d.toLocaleTimeString()}
+          <i className="fas fa-archive"></i>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Header;
+export default Calls;
